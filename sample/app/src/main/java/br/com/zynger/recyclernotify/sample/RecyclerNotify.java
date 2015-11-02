f90b1daf656961e8c78cba843e61d1a10041107d
julioz <julioz92@gmail.com>
2015-11-02 14:08:22 -0200

    Add component background with round corners and color selector

--- sample/app/src/main/java/br/com/zynger/recyclernotify/sample/RecyclerNotify.java
+++ sample/app/src/main/java/br/com/zynger/recyclernotify/sample/RecyclerNotify.java
@@ -5,18 +5,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class RecyclerNotify {
 
     private Context mContext;
-    private LayoutInflater mLayoutInflater;
+    private RelativeLayout mWrappingLayout;
     private RecyclerView mRecyclerView;
+    private ImageView mIconImageView;
+    private TextView mTitleTextView;
 
     public void attach(RecyclerView recyclerView) {
         mRecyclerView = recyclerView;
         mContext = mRecyclerView.getContext();
-        mLayoutInflater = LayoutInflater.from(mContext);
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        mWrappingLayout = (RelativeLayout) layoutInflater.inflate(R.layout.component_recyclernotify, null);
+        mIconImageView = (ImageView) mWrappingLayout.findViewById(R.id.rn_icon);
+        mTitleTextView = (TextView) mWrappingLayout.findViewById(R.id.rn_text);
 
         ViewParent recyclerViewParent = recyclerView.getParent();
         if (recyclerViewParent instanceof ViewGroup) {
@@ -24,12 +31,14 @@ public class RecyclerNotify {
             recyclerViewParentViewGroup.removeView(recyclerView);
 
             attachInViewGroup(recyclerViewParentViewGroup);
+        } else {
+            throw new RuntimeException("You can only attach a " +
+                    RecyclerNotify.class.getSimpleName() + " to a ViewGroup!");
         }
     }
 
     private void attachInViewGroup(ViewGroup viewGroup) {
-        RelativeLayout wrappingLayout = (RelativeLayout) mLayoutInflater.inflate(R.layout.component_recyclernotify, null);
-        wrappingLayout.addView(mRecyclerView, 0);
-        viewGroup.addView(wrappingLayout);
+        mWrappingLayout.addView(mRecyclerView, 0);
+        viewGroup.addView(mWrappingLayout);
     }
 }
