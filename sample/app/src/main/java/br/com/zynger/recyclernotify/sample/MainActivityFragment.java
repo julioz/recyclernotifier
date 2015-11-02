7c8f21727da5eacd4acb8d5a4ee3d819bdfd5a97
julioz <julioz92@gmail.com>
2015-11-02 13:18:00 -0200

    Add simple recycler view with adapter

--- sample/app/src/main/java/br/com/zynger/recyclernotify/sample/MainActivityFragment.java
+++ sample/app/src/main/java/br/com/zynger/recyclernotify/sample/MainActivityFragment.java
@@ -1,15 +1,20 @@
 package br.com.zynger.recyclernotify.sample;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivityFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
 
     public MainActivityFragment() {
     }
@@ -17,6 +22,52 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            items.add("Item " + String.valueOf(i));
+        }
+        SampleAdapter adapter = new SampleAdapter(items);
+        mRecyclerView.setAdapter(adapter);
+        return view;
+    }
+
+    private class SampleAdapter extends RecyclerView.Adapter<SampleAdapter.ViewHolder> {
+        private List<String> mItems;
+
+        public SampleAdapter(List<String> items) {
+            this.mItems = items;
+        }
+
+        @Override
+        public SampleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemLayoutView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_layout, null);
+            return new ViewHolder(itemLayoutView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+            viewHolder.mTextView.setText(mItems.get(position));
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+
+            public ViewHolder(View itemLayoutView) {
+                super(itemLayoutView);
+                mTextView = (TextView) itemLayoutView.findViewById(R.id.item_title);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
     }
 }
